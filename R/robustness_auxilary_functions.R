7f7991c30bb5443f05d95e9008dab8b8d55f5d51
Michael Gurkov <michael.gurkov@mail.huji.ac.il>
2023-02-04 21:04:44 +0200

    update lit review and move mathiching by size to different function

--- R/robustness_auxilary_functions.R
+++ R/robustness_auxilary_functions.R
@@ -1,4 +1,58 @@
-#' This function matches
+#' This function matches comps by market value
+#'
+match_comps_by_market_value = function(ipo_comps, market_df,
+                                       age_threshold = 1,
+                                       market_threshold = 0.1,
+                                       single_match = TRUE){
+
+
+  ipo_sample = market_df %>%
+    filter(!is.na(market_value)) %>%
+    inner_join(ipo_comps, by = "tase_id") %>%
+    unite(id,c("tase_id","sec_id")) %>%
+    group_by(id) %>%
+    summarise(market_value = market_value[date == min(date)],
+              date = min(date),
+              .groups = "drop")
+
+  target_sample = market_df %>%
+    filter(!is.na(market_value)) %>%
+    unite(id, c("tase_id","sec_id")) %>%
+    select(date, id, market_value) %>%
+    group_by(id) %>%
+    mutate(age = as.numeric(date - min(date)) / 252) %>%
+    ungroup()
+
+
+  matched_sample = ipo_sample %>%
+    left_join(target_sample, by = c("date"),
+              suffix = c("_ipo","_control")) %>%
+    filter(complete.cases(.)) %>%
+    filter(age >= age_threshold) %>%
+    mutate(matching_diff = abs(market_value_ipo/market_value_control - 1)) %>%
+    filter(matching_diff < market_threshold)
+
+  if(single_match){
+
+    matched_sample = matched_sample %>%
+      group_by(id_ipo) %>%
+      slice_min(order_by = matching_diff) %>%
+      ungroup() %>%
+      select(id_ipo, id_control)
+
+
+  } else {
+
+    matched_sample = matched_sample %>%
+       select(id_ipo, id_control)
+
+
+  }
+
+ return(matched_sample)
+
+
+}
 
 
 
