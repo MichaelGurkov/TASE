3e6194f9adf0e7b8f79772db7220cf6399bf867b
Michael Gurkov <Michael.Gurkov@mail.huji.ac.il>
2023-02-22 20:57:54 +0200

    bugfix bhar calculation (average by id) and add period returns plot

--- R/robustness_auxilary_functions.R
+++ R/robustness_auxilary_functions.R
@@ -87,3 +87,38 @@ calculate_bhar_from_price_df = function(price_df,
 
 
 }
+
+
+#' This function calculates periodic returns from price df. In order to do that
+#' the frequency is reduced by taking the endpoints of the period
+#'
+calculate_period_ret_from_price_df = function(price_df,
+                                                  period_freq,
+                                                  period_thresh = NULL){
+
+  period_df = price_df %>%
+    group_by(id,id_control) %>%
+    arrange(date) %>%
+    mutate(duration = as.numeric(date - min(date))) %>%
+    mutate(period = duration %/% period_freq) %>%
+    {if(!is.null(period_thresh)) filter(., period <= period_thresh) else .} %>%
+    group_by(period) %>%
+    slice_min(duration) %>%
+    ungroup()
+
+
+  ret_df = period_df %>%
+    group_by(id, id_control) %>%
+    arrange(period) %>%
+    mutate(across(starts_with("price"), ~. / lag(.) - 1)) %>%
+    ungroup() %>%
+    rename_with(.cols = starts_with("price"),
+                ~str_replace(.,"price","ret")) %>%
+    select(id, id_control,period, starts_with("ret")) %>%
+    filter(complete.cases(.)) %>%
+    mutate(ret_abnormal = ret_ipo - ret_control)
+
+  return(ret_df)
+
+
+}
